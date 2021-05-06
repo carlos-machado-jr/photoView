77fb72b1d68712fb1ba3ddeae03c6cfc33d23fa0
Técnico Informática <51121742019.1@senacrjedu.local>
2021-05-06 19:43:21 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModulesModule } from './modules/modules.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ModulesModule,
+    CoreModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/modules/modules-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FotoComponent } from './foto/foto.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { FormularioComponent } from './formulario/formulario.component';
+
+const routes: Routes = [
+  {path: '', component: FotoComponent},
+  {path: 'sobre', component: SobreComponent},
+  {path: 'formulario', component: FormularioComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ModulesRoutingModule { }
