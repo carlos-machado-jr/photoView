edf61f0f683f4e0fc6f673dec17633076d6888bd
Técnico Informática <51121742019.1@senacrjedu.local>
2021-05-06 21:13:48 -0300

    modificado a estrutura da pasta modules, cada pagina tem seu modulo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+    
+  { path: 'fotos', loadChildren: () => import('./modules/fotos/fotos.module').then(m => m.FotosModule) },
+  { path: 'sobre', loadChildren: () => import('./modules/sobre/sobre.module').then(m => m.SobreModule) },
+  { path: 'formulario', loadChildren: () => import('./modules/formulario/formulario.module').then(m => m.FormularioModule) },
   {path: '', 
-    pathMatch:'full', 
-    redirectTo: '/fotos'
-  },
-  {path: 'fotos', loadChildren: () => import('./modules/modules.module').then(m => m.ModulesModule)}
+   pathMatch:'full', 
+   redirectTo: '/fotos'
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ModulesModule } from './modules/modules.module';
 import { CoreModule } from './core/core.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -13,7 +12,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    ModulesModule,
     CoreModule,
     AppRoutingModule,
     NgbModule
