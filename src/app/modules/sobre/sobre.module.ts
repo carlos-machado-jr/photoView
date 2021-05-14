9ad7f92b28cbbbd98e996087d1c9fbfb880b5b2a
Técnico Informática <51121742019.1@senacrjedu.local>
2021-05-13 21:16:59 -0300

    criacao do servico firebase

--- src/app/modules/sobre/sobre.module.ts
+++ src/app/modules/sobre/sobre.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { SobreRoutingModule } from './sobre-routing.module';
 import { SobreComponent } from './sobre.component';
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatFormField } from '@angular/material/form-field';
+import { MatOptionModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
 
 
 @NgModule({
@@ -11,7 +16,13 @@ import { SobreComponent } from './sobre.component';
   ],
   imports: [
     CommonModule,
-    SobreRoutingModule
+    SobreRoutingModule,
+    MatSidenavModule,
+    MatFormField,
+    MatOptionModule,
+    MatButtonModule,
+    MatSelectModule,
+    NgModule
   ]
 })
 export class SobreModule { }
